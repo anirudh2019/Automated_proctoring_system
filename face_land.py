a13b4dd8dd18a41b56365eb3eef51229318740eb
Anirudh <iit2019068@iiita.ac.in>
2021-10-16 15:33:51 +0530

    Added FaceRecog, major changes in code
    Now every task has own file.

---
+++ face_land.py
@@ -0,0 +1,35 @@
+from face_land_mp import face_landmarks_mp
+from imutils import face_utils
+import numpy as np
+import cv2
+import dlib
+import utils
+
+# Facial landmarks predictor
+saved_model = "models/shape_predictor_68_face_landmarks.dat"
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(saved_model)
+
+def detect_landmarks(frame, faces, module = "Dlib"):
+    shape = []
+   
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    if module == "Dlib":
+        # loop over the face detections
+        for (i, face) in enumerate(faces):
+            # determine the facial landmarks for the face region, then
+            # convert the facial landmark (x, y)-coordinates to a NumPy
+            # array
+            shape = predictor(gray, face)
+            shape = face_utils.shape_to_np(shape)
+        
+            # loop over the (x, y)-coordinates for the facial landmarks
+            # and draw them on the frame
+            if draw:
+                for (x, y) in shape:
+                    cv2.circle(frame, (x, y), 2, (0, 255, 0), -1)      
+    elif module == "mediapipe":
+        ret, shape, frame = face_landmarks_mp(frame)
+    
+    return False, shape, frame
--- face_recog.py
+++ face_recog.py
@@ -1,73 +1,39 @@
-import face_recognition
+from facenet import construct_facenet
 import cv2
-import os
-import glob
+import tensorflow as tf
 import numpy as np
 
-class Face_recog:
-    def __init__(self):
-        self.known_face_encodings = []
-        self.known_face_names = []
-
-        # Resize frame for a faster speed
-        self.frame_resizing = 0.25
-
-    def load_encoding_images(self, images_path):
-        """
-        Load encoding images from path
-        :param images_path:
-        :return:
-        """
-        # Load Images
-        images_path = glob.glob(os.path.join(images_path, "*.*"))
-
-        print("{} encoding images found.".format(len(images_path)))
-
-        # Store image encoding and names
-        for img_path in images_path:
-            img = cv2.imread(img_path)
-            rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-            # Get the filename only from the initial file path.
-            basename = os.path.basename(img_path)
-            (filename, ext) = os.path.splitext(basename)
-            # Get encoding
-            img_encoding = face_recognition.face_encodings(rgb_img)[0]
-
-            # Store file name and file encoding
-            self.known_face_encodings.append(img_encoding)
-            self.known_face_names.append(filename)
-        print("Encoding images loaded")
-
-    def detect_known_faces(self, frame):
-        small_frame = cv2.resize(frame, (0, 0), fx=self.frame_resizing, fy=self.frame_resizing)
-                
-        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-        rgb_small_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
+class Recognizer:
+    def __init__(self, threshold = 0.8):
+        self.model = construct_facenet()
+        self.input_embeddings = None
+        self.threshold = threshold
         
-        # Find all the faces and face encodings in the current frame of video
-        face_locations = face_recognition.face_locations(rgb_small_frame)
-        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
-
-        face_names = []
-        for face_encoding in face_encodings:
-            # See if the face is a match for the known face(s)
-            matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
-            name = "Unknown"
-
-            # # If a match was found in known_face_encodings, just use the first one.
-            # if True in matches:
-            #     first_match_index = matches.index(True)
-            #     name = known_face_names[first_match_index]
-
-            # Or instead, use the known face with the smallest distance to the new face
-            face_distances = face_recognition.face_distance(self.known_face_encodings, face_encoding)
-            best_match_index = np.argmin(face_distances)
-            if matches[best_match_index]:
-                name = self.known_face_names[best_match_index]
-            face_names.append(name)
-
-        # Convert to numpy array to adjust coordinates with frame resizing quickly
-        face_locations = np.array(face_locations)
-        face_locations = face_locations / self.frame_resizing
-        return face_locations.astype(int), face_names
+    def face_to_embedding(self,face): 
+        image = cv2.resize(face, (96, 96)) 
+        img = image[...,::-1]
+        img = np.around(np.transpose(img, (0,1,2))/255.0, decimals=12)
+        x_train = np.array([img])
+        embedding = self.model.predict_on_batch(x_train)
+        return embedding
+        
+    def recognize_face(self, face):
+        embedding = self.face_to_embedding(face)
+        minimum_distance = 200 # just initiating min variable
+        name = None
+        # Loop over  names and encodings.
+        for (input_name, input_embedding) in self.input_embeddings.items():
+            euclidean_distance = np.linalg.norm(embedding-input_embedding)
+            if euclidean_distance < minimum_distance:
+                minimum_distance = euclidean_distance
+                name = input_name
+    
+        if minimum_distance < self.threshold:
+            return str(name), minimum_distance
+        else:
+            return "Unknown", minimum_distance
+    
+    def verify_faces(self, faces):
+        for face in faces:
+            face.name, face.distance = self.recognize_face(face.img)
+        return
